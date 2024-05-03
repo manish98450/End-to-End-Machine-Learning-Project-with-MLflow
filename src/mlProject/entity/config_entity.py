bfe67b118efaffbfcd654229105afdad4b239a14
Manish Pandey <mp9080952@gmail.com>
2024-05-03 12:08:52 +0530

    model trainer added

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -18,13 +18,12 @@ class DataValidationConfig:
     unzip_data_dir: Path
     all_schema: dict
 
-'''
+
 
 @dataclass(frozen=True)
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path
-
 
 
 @dataclass(frozen=True)
@@ -36,19 +35,3 @@ class ModelTrainerConfig:
     alpha: float
     l1_ratio: float
     target_column: str
-
-
-
-
-@dataclass(frozen=True)
-class ModelEvaluationConfig:
-    root_dir: Path
-    test_data_path: Path
-    model_path: Path
-    all_params: dict
-    metric_file_name: Path
-    target_column: str
-    mlflow_uri: str
-    
-    
-'''
